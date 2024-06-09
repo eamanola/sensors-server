380fabb5b956d9cdbac1e78d0adcd58a56acdef3
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2024-06-09 20:58:44 +0300

    pylint

--- sensordata.py
+++ sensordata.py
@@ -3,104 +3,83 @@ import re
 
 
 def cpu_model():
-  try:
-    result = subprocess.run(
-      ["lscpu | grep 'Model name:' |sed 's/^Model\ name:\ *//'"],
-      stdout = subprocess.PIPE,
-      shell = True,
-      universal_newlines = True,
-    )
+  result = subprocess.run(
+    [r"lscpu | grep 'Model name:' |sed 's/^Model\ name:\ *//'"],
+    stdout = subprocess.PIPE,
+    shell = True,
+    universal_newlines = True,
+    check = True,
+  )
 
-    return result.stdout.strip();
-
-  except Exception as err:
-    print("cpu_model:", err);
-    return None;
+  return result.stdout.strip()
 
 
 def gpu_model():
-  try:
-    result = subprocess.run(
-      ["nvidia-smi", "--query-gpu", "gpu_name", "--format=csv,noheader"],
-      stdout = subprocess.PIPE,
-      universal_newlines = True,
-    );
+  result = subprocess.run(
+    ["nvidia-smi", "--query-gpu", "gpu_name", "--format=csv,noheader"],
+    stdout = subprocess.PIPE,
+    universal_newlines = True,
+    check = True,
+  )
 
-    return result.stdout.strip();
-
-  except Exception as err:
-    print("gpu_model:", err);
-    return None;
+  return result.stdout.strip()
 
 
 def cpu_usage():
-  try:
-    result = subprocess.run(
-      ["vmstat 1 2|tail -1|awk '{print $15}'"],
-      stdout = subprocess.PIPE,
-      universal_newlines = True,
-      shell = True
-    );
+  result = subprocess.run(
+    ["vmstat 1 2|tail -1|awk '{print $15}'"],
+    stdout = subprocess.PIPE,
+    universal_newlines = True,
+    shell = True,
+    check = True,
+  )
 
-    return 100 - int(result.stdout);
+  return 100 - int(result.stdout)
 
-  except Exception as err:
-    print("cpu_usage:", err);
-    return None;
 
 # too Spaghetti
 # requires 2nd subprocess call for cpu temp
 #def fanspeeds():
-#  try:
-#    result = subprocess.run(
-#      ["sensors -u|grep -B 1 -E 'fan.+_input'|sed 's/.*_input:\s*//'|sed s/://"],
-#      stdout = subprocess.PIPE,
-#      universal_newlines = True,
-#      shell = True
-#    );
+#  result = subprocess.run(
+#    ["sensors -u|grep -B 1 -E 'fan.+_input'|sed 's/.*_input:\s*//'|sed s/://"],
+#    stdout = subprocess.PIPE,
+#    universal_newlines = True,
+#    shell = True
+#  )
 #
-#    fanlist = list(map(
-#      (lambda fan: { "name": fan[0], "speed": fan[1], "unit": "rpm" }),
-#      [line.split('\n') for line in result.stdout.strip().split('\n--\n')]
-#    ));
-#    cpu_fan = fanlist.pop(1);
+#  fanlist = list(map(
+#    (lambda fan: { "name": fan[0], "speed": fan[1], "unit": "rpm" }),
+#    [line.split('\n') for line in result.stdout.strip().split('\n--\n')]
+#  ))
+#  cpu_fan = fanlist.pop(1)
 #
-#    return cpu_fan, fanlist;
-#  except Exception as err:
-#
-#    print("sensors:", err);
-#    return None;
-
+#  return cpu_fan, fanlist
 
 def sensors():
-  try:
-    result = subprocess.run(
-      ["sensors", "-u"],
-      stdout = subprocess.PIPE,
-      universal_newlines = True,
-    );
+  result = subprocess.run(
+    ["sensors", "-u"],
+    stdout = subprocess.PIPE,
+    universal_newlines = True,
+    check = True,
+  )
 
-    return result.stdout.strip().split('\n');
-
-  except Exception as err:
-    print("sensors:", err);
-    return None;
+  return result.stdout.strip().split('\n')
 
 
 def cpu_temp(sensors_output):
   for i, item in enumerate(sensors_output):
-    if(item == "Tctl:"):
+    if item == "Tctl:":
       return float(re.sub(r'^\s*temp1_input:\s*', '', sensors_output[i + 1]))
 
-  return 0;
+  return 0
 
 
 def fanspeeds(sensors_output):
   fans = []
-  reg = re.compile('^\s*fan\d+_input:\s*')
+  reg = re.compile(r'^\s*fan\d+_input:\s*')
 
   for i, item in enumerate(sensors_output):
-    if (reg.match(item)):
+    if reg.match(item):
       fans.append({
         "name": re.sub(r':\s*$', '', sensors_output[i - 1]),
         "speed": (float(reg.sub('', item)), "rpm")
@@ -110,49 +89,39 @@ def fanspeeds(sensors_output):
 
 
 def gpu_info():
-  try:
-    keys = [
-      "utilization.gpu",
-      "utilization.memory",
-      "temperature.gpu",
-      "fan.speed",
-    ];
+  keys = [
+    "utilization.gpu",
+    "utilization.memory",
+    "temperature.gpu",
+    "fan.speed",
+  ]
 
-    result = subprocess.run(
-      [
-        "nvidia-smi",
-        "--query-gpu",
-        ','.join(keys),
-        "--format=csv,noheader,nounits",
-      ],
-      stdout = subprocess.PIPE,
-      universal_newlines = True,
-    );
+  result = subprocess.run(
+    [
+      "nvidia-smi",
+      "--query-gpu",
+      ','.join(keys),
+      "--format=csv,noheader,nounits",
+    ],
+    stdout = subprocess.PIPE,
+    universal_newlines = True,
+    check = True,
+  )
 
-    return [int(i) for i in result.stdout.strip().split(', ')];
-
-  except Exception as err:
-    print("gpu_info:", err);
-    return None;
+  return [int(i) for i in result.stdout.strip().split(', ')]
 
 
-cpu_name = None;
-gpu_name = None;
+CPU_NAME = cpu_model()
+GPU_NAME = gpu_model()
 
 
 def sensor_data():
-  global cpu_name;
-  global gpu_name;
+  cpu_utilization = cpu_usage()
+  gpu_utilization, gpu_memory_utilization, gpu_temperature, gpu_fanspeed = gpu_info()
 
-  cpu_name = cpu_model() if cpu_name is None else cpu_name;
-  gpu_name = gpu_model() if gpu_name is None else gpu_name;
-
-  cpu_utilization = cpu_usage();
-  gpu_utilization, gpu_memory_utilization, gpu_temperature, gpu_fanspeed = gpu_info();
-
-  sensors_output = sensors();
-  cpu_temperture = cpu_temp(sensors_output);
-  fans = fanspeeds(sensors_output);
+  sensors_output = sensors()
+  cpu_temperture = cpu_temp(sensors_output)
+  fans = fanspeeds(sensors_output)
 
   # system specific
   cpu_fan = fans.pop(1)
@@ -167,17 +136,17 @@ def sensor_data():
   # system specific
 
   cpu = {
-    "name": cpu_name,
+    "name": CPU_NAME,
     "utilization": (cpu_utilization, "%"),
     "temperature": (cpu_temperture, "°C"),
     "fanspeed": (cpu_fan["speed"]),
-  };
+  }
   gpu = {
-    "name": gpu_name,
+    "name": GPU_NAME,
     "utilization": (gpu_utilization, "%"),
     "memory": (gpu_memory_utilization, "%"),
     "temperature": (gpu_temperature, "°C"),
     "fanspeed": (gpu_fanspeed, "%"),
-  };
+  }
 
-  return cpu, gpu, fans;
+  return cpu, gpu, fans
--- server.py
+++ server.py
@@ -1,21 +1,23 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+import subprocess
 from sensordata import sensor_data
 
-# hostname = "localhost"
-hostname = "0.0.0.0"
-port = 8080
+# HOSTNAME = "localhost"
+HOSTNAME = "0.0.0.0"
+PORT = 8080
 
 def json_sensor_data():
-  cpu, gpu, fans = sensor_data();
+  cpu, gpu, fans = sensor_data()
 
   return json.dumps({
     "cpu": cpu,
     "gpu": gpu,
     "fans": fans,
-  });
+  })
 
 class SensorsServer(BaseHTTPRequestHandler):
+  # pylint: disable-next=C0103:invalid-name
   def do_GET(self):
     if self.path == "/health":
       self.send_response(200)
@@ -25,18 +27,21 @@ class SensorsServer(BaseHTTPRequestHandler):
 
     elif self.path == "/sensors":
       try:
-        data = json_sensor_data();
+        data = json_sensor_data()
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
         self.wfile.write(bytes(data, "UTF-8"))
 
-      except Exception as err:
+      except (
+        OSError,
+        subprocess.CalledProcessError,
+        subprocess.TimeoutExpired,
+      ) as err:
+        print("/sensors:", err)
         self.send_response(500)
         self.end_headers()
         self.wfile.write(bytes("Internal Server Error", "UTF-8"))
-
-        print(err)
 
     else:
       self.send_response(403)
@@ -44,8 +49,8 @@ class SensorsServer(BaseHTTPRequestHandler):
       self.wfile.write(bytes("Forbidden", "UTF-8"))
 
 if __name__ == "__main__":
-  sensorsServer = HTTPServer((hostname, port), SensorsServer)
-  print(f'Server started http://{hostname}:{port}')
+  sensorsServer = HTTPServer((HOSTNAME, PORT), SensorsServer)
+  print(f'Server started http://{HOSTNAME}:{PORT}')
 
   try:
     sensorsServer.serve_forever()
