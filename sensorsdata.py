08f4e9675d5a0161282a90c17c3652ed85ae16da
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2024-06-10 14:46:45 +0300

    use js style objs

--- sensorsdata.py
+++ sensorsdata.py
@@ -2,7 +2,7 @@ import subprocess
 import re
 
 
-def cpu_model():
+def get_cpu_model():
   result = subprocess.run(
     ["lscpu"],
     stdout = subprocess.PIPE,
@@ -17,10 +17,10 @@ def cpu_model():
 
   return reg.sub("", model_lines[0])
 
-CPU_NAME = cpu_model()
+CPU_NAME = get_cpu_model()
 
 
-def gpu_model():
+def get_gpu_model():
   result = subprocess.run(
     ["nvidia-smi", "--query-gpu", "gpu_name", "--format=csv,noheader"],
     stdout = subprocess.PIPE,
@@ -30,11 +30,11 @@ def gpu_model():
 
   return result.stdout.strip()
 
-GPU_NAME = gpu_model()
+GPU_NAME = get_gpu_model()
 
 
-def gpu_info():
-  keys = ["utilization.gpu", "utilization.memory", "temperature.gpu", "fan.speed"]
+def get_gpu_info():
+  keys = ["utilization.gpu", "temperature.gpu", "fan.speed", "utilization.memory"]
 
   result = subprocess.run(
     ["nvidia-smi", "--query-gpu", ",".join(keys), "--format=csv,noheader,nounits"],
@@ -43,20 +43,9 @@ def gpu_info():
     check = True,
   )
 
-  return dict(zip(keys, [int(i) for i in result.stdout.strip().split(", ")]))
+  return [int(i) for i in result.stdout.strip().split(", ")]
 
-
-def format_gpu(info):
-  return {
-    "name": GPU_NAME,
-    "utilization": [info["utilization.gpu"], "%"],
-    "memory": [info["utilization.memory"], "%"],
-    "temperature": [info["temperature.gpu"], "°C"],
-    "fanspeed": [info["fan.speed"], "%"],
-  }
-
-
-def sensors():
+def get_sensors_info():
   result = subprocess.run(
     ["sensors", "-u"],
     stdout = subprocess.PIPE,
@@ -67,41 +56,7 @@ def sensors():
   return result.stdout.strip().split("\n")
 
 
-def get_fans(sensors_data):
-  fans = []
-  reg = re.compile(r"^\s*fan\d+_input:\s*")
-
-  for i, item in enumerate(sensors_data):
-    if reg.match(item):
-      fans.append({
-        "name": re.sub(r":\s*$", "", sensors_data[i - 1]),
-        "speed": [float(reg.sub("", item)), "rpm"]
-      })
-
-  return fans
-
-
-# system specific
-def format_fans(fans):
-  # cpu fan#2
-  cpu_fan = fans.pop(1)
-  assert cpu_fan["name"] == "CPU_FAN"
-
-  for fan in fans:
-    # Rear fans connected to CHA_FAN1 header
-    if fan["name"] == "CHA_FAN1":
-      fan["name"] = "Rear"
-      continue
-
-    # Front fans connected to CHA_FAN2 header
-    if fan["name"] == "CHA_FAN2":
-      fan["name"] = "Front"
-      continue
-
-  return fans, cpu_fan
-
-
-def cpu_usage():
+def get_cpu_usage():
   result = subprocess.run(
     ["vmstat 1 2|tail -1|awk '{print $15}'"],
     stdout = subprocess.PIPE,
@@ -113,49 +68,94 @@ def cpu_usage():
   return 100 - int(result.stdout)
 
 
-def cpu_temp(sensors_data):
-  for i, item in enumerate(sensors_data):
+# fans can be named in /etc/sensors.d/*.conf
+def take_cpu_fan(fans):
+  copy = list(fans)
+
+  cpu_fan = copy.pop(1)
+  assert cpu_fan[0] == "CPU_FAN"
+
+  return copy, cpu_fan
+
+
+def parse_fans(sensors_info):
+  fans = []
+  reg = re.compile(r"^\s*fan\d+_input:\s*")
+
+  for i, item in enumerate(sensors_info):
+    if reg.match(item):
+      fans.append([
+        re.sub(r":\s*$", "", sensors_info[i - 1]),  # name
+        float(reg.sub("", item)),                   # speed
+      ])
+
+  return take_cpu_fan(fans)
+
+
+def parse_cpu_temp(sensors_info):
+  for i, item in enumerate(sensors_info):
     if item == "Tctl:":
-      return float(re.sub(r"^\s*temp1_input:\s*", "", sensors_data[i + 1]))
+      return float(re.sub(r"^\s*temp1_input:\s*", "", sensors_info[i + 1]))
 
   return 0
 
 
-def get_cpu(sensors_data, cpu_fan):
+def relabel_fans(fans):
+  copy = list(fans)
+
+  for fan in copy:
+    # Rear fans connected to CHA_FAN1 header
+    if fan[0] == "CHA_FAN1":
+      fan[0] = "Rear"
+      continue
+
+    # Front fans connected to CHA_FAN2 header
+    if fan[0] == "CHA_FAN2":
+      fan[0] = "Front"
+      continue
+
+  return copy
+
+
+def format_cpu(cpu_usage, cpu_temp, cpu_fan):
   return {
     "name": CPU_NAME,
-    "utilization": [cpu_usage(), "%"],
-    "temperature": [cpu_temp(sensors_data), "°C"],
-    "fanspeed": cpu_fan["speed"],
+    "utilization": { "value": cpu_usage, "unit": "%" },
+    "temperature": { "value": cpu_temp, "unit": "°C" },
+    "fanspeed": { "value": cpu_fan[1], "unit": "rpm" },
   }
 
 
-# too Spaghetti
-# requires 2nd subprocess call for cpu temp
-#def fanspeeds():
-#  result = subprocess.run(
-#    ["sensors -u|grep -B 1 -E 'fan.+_input'|sed 's/.*_input:\s*//'|sed s/://"],
-#    stdout = subprocess.PIPE,
-#    universal_newlines = True,
-#    shell = True
-#  )
-#
-#  fanlist = list(map(
-#    (lambda fan: { "name": fan[0], "speed": fan[1], "unit": "rpm" }),
-#    [line.split('\n') for line in result.stdout.strip().split('\n--\n')]
-#  ))
-#  cpu_fan = fanlist.pop(1)
-#
-#  return cpu_fan, fanlist
+def format_gpu(gpu_usage, gpu_temp, gpu_fanspeed, gpu_memory):
+  return {
+    "name": GPU_NAME,
+    "utilization": { "value": gpu_usage, "unit": "%" },
+    "temperature": { "value": gpu_temp, "unit": "°C" },
+    "fanspeed": { "value": gpu_fanspeed, "unit": "%" },
+    "memory": { "value": gpu_memory, "unit": "%" },
+  }
 
+
+def format_fans(fans):
+  return [
+    { "name": fan[0], "speed": { "value": fan[1], "unit": "rpm" } }
+    for fan in fans
+  ]
+
+# sensors -u|grep -B 1 -E 'fan.+_input'|sed 's/.*_input:\s*//'|sed s/://
 
 def get_sensors():
-  gpu = format_gpu(gpu_info())
+  sensors_info = get_sensors_info()
+  rest_of_fans, cpu_fan = parse_fans(sensors_info)
 
-  sensors_data = sensors()
+  cpu_usage = get_cpu_usage()
+  cpu_temp = parse_cpu_temp(sensors_info)
+  cpu = format_cpu(cpu_usage, cpu_temp, cpu_fan)
 
-  fans, cpu_fan = format_fans(get_fans(sensors_data))
+  gpu_usage, gpu_temp, gpu_fanspeed, gpu_memory = get_gpu_info()
+  gpu = format_gpu(gpu_usage, gpu_temp, gpu_fanspeed, gpu_memory)
 
-  cpu = get_cpu(sensors_data, cpu_fan)
+  relabeled = relabel_fans(rest_of_fans)
+  fans = format_fans(relabeled)
 
   return cpu, gpu, fans
